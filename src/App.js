2874e7245c25ac4508de546f7225762fe936f5b4
evastolz <eva.stolz@doctolib.com>
2020-05-05 16:37:47 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+const Route = ({ path, component: Component }) =>
+  window.location.pathname === path ? <Component /> : null;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <a href="/">Home</a>
+      </div>
+      <div>
+        <a href="/about">About</a>
+      </div>
+      <div>
+        <a href="/contact">Contact</a>
+      </div>
+      <div>
+        <Route path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
+      </div>
+    </>
   );
 }
+
+const Home = () => <div>This is Home</div>;
+
+const About = () => <div>This is About me</div>;
+
+const Contact = () => <div>This is Contact me</div>;
 
 export default App;
