2b06f8aadeba751d59befbd84ee1f11de1c8bf80
evastolz <eva.stolz@doctolib.com>
2020-05-19 18:42:05 +0200

    finish

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from "react";
+import React, { useContext } from "react";
 
-const Route = ({ path, component: Component }) =>
-  window.location.pathname === path ? <Component /> : null;
+const PathContext = React.createContext();
 
-const Link = ({ children, to }) => (
-  <div>
-    <a href={to}>{children}</a>
-  </div>
-);
+const Route = ({ path, component: Component }) => {
+  const { path: currentPath } = useContext(PathContext);
+  return currentPath === path ? <Component /> : null;
+};
+
+const Link = ({ children, to }) => {
+  const { setPath } = useContext(PathContext);
+
+  return (
+    <div>
+      <a
+        href={to}
+        onClick={(e) => {
+          e.preventDefault();
+          setPath(to);
+        }}
+      >
+        {children}
+      </a>
+    </div>
+  );
+};
+
+const BrowserRouter = ({ children }) => {
+  const [path, setPath] = React.useState(window.location.pathname);
+
+  React.useEffect(() => {
+    window.history.pushState(null, null, path);
+  }, [path]);
+
+  return (
+    <PathContext.Provider value={{ path, setPath }}>
+      {children}
+    </PathContext.Provider>
+  );
+};
+
+// ----------------
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Link to="/">Home</Link>
       <Link to="/about">About</Link>
       <Link to="/contact">Contact</Link>
       <div>
         <Route path="/" component={Home} />
         <Route path="/about" component={About} />
+        <Route path="/about" component={About} />
         <Route path="/contact" component={Contact} />
       </div>
-    </>
+    </BrowserRouter>
   );
 }
 
